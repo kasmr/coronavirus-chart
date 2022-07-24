929769c5d64b11c6f47861bb42cdcb747a7cf5d9
kasmr <alikkras@gmail.com>
2022-07-25 01:09:13 +0300

    Changed ThemeSwitcher icon

--- src/components/ThemeSwitcher/ThemeSwitcher.js
+++ src/components/ThemeSwitcher/ThemeSwitcher.js
@@ -1,24 +1,24 @@
 import React, { useContext } from 'react';
 
 import Checkbox from '@material-ui/core/Checkbox';
-import Brightness7Icon from '@material-ui/icons/Brightness7';
+import WbSunnyIcon from '@material-ui/icons/WbSunny';
+import Brightness3Icon from '@material-ui/icons/Brightness3';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Brightness4OutlinedIcon from '@material-ui/icons/Brightness4Outlined';
 
 import { GlobalContext } from '../../context/GlobalState';
 
 
 const ThemeSwitcher = () => {
 
-    const { themeType, switchThemeType } = useContext(GlobalContext);
+    const { theme, switchThemeType } = useContext(GlobalContext);
 
     return (
         <FormControlLabel label="" className="switch" control={
             <Checkbox color="default"
                       onChange={switchThemeType}
-                      checked={themeType === 'light'}
-                      icon={<Brightness4OutlinedIcon style={{ fontSize: 40 }}/>}
-                      checkedIcon={<Brightness7Icon style={{ fontSize: 40 }} color="inherit"/>}/>
+                      checked={theme === 'dark'}
+                      icon={<Brightness3Icon style={{ fontSize: 40 }} color="inherit"/>}
+                      checkedIcon={<WbSunnyIcon style={{ fontSize: 40 }}/>}/>
         }/>
     );
 };
